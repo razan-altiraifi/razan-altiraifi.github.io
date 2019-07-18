46d953aaded631d163b93f4911ef89a4987e1c06
razan-altiraifi <52507311+razan-altiraifi@users.noreply.github.com>
2019-07-18 14:53:52 -0400

    Update events.js

--- events.js
+++ events.js
@@ -7,7 +7,10 @@ AFRAME.registerComponent('markerhandler', {
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
+            
+			window.open("https://www.perkinswill.com", "_system");
+			
+			if (aEntity && intersectedElement === aEntity) {
                 const scale = aEntity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
                 aEntity.setAttribute('scale', scale);
